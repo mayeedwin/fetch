bb0c7a43a8f4b51b89dd381ed5a327c8b3f56fb5
Glitch (javascript-fetch) <none>
2019-11-19 19:00:49 +0000

    👌🎳 Updated with Glitch

--- App.js
+++ App.js
@@ -1,35 +1,41 @@
-import { getData } from './modules/Fetch.js';
+import { getData } from "./modules/Fetch.js";
 
-getData().then(data => {
+getData()
+  .then(data => {
     let users = data;
-
-    let all = document.querySelector('.users');
+    let all = document.querySelector(".users");
 
     // loop through users
     for (let user of users) {
-      
-        let person = document.createElement('div');
-        let name = document.createElement('p');
-        let bio = document.createElement('p');
-        let country = document.createElement('span');
+      // create HTML5 elements
+      let person = document.createElement("div");
+      let username = document.createElement("span");
+      let name = document.createElement("p");
+      let bio = document.createElement("p");
+      let country = document.createElement("span");
 
-        // set attributes
-        name.setAttribute('class', 'name');
-        bio.setAttribute('class', 'bio');
-        country.setAttribute('class', 'country');
-       
-        // set inner text
-        name.innerText = `${user.name}`;
-        bio.innerText = `${user.bio}`;
-        country.innerText = `${user.country}`;
+      // set attributes
+      person.setAttribute("class", "person");
+      name.setAttribute("class", "name");
+      username.setAttribute("class", "username");
+      bio.setAttribute("class", "bio");
+      country.setAttribute("class", "country");
 
-        all.appendChild(person);
-        person.appendChild(name);
-        person.appendChild(bio);
-        person.appendChild(country);
+      // set inner text
+      name.innerText = `${user.name}`;
+      username.innerText = `@${user.username}`;
+      bio.innerText = `${user.bio}`;
+      country.innerText = `${user.country}`;
+
+      // append elements for rendering
+      all.appendChild(person);
+      person.appendChild(name);
+      person.appendChild(username);
+      person.appendChild(bio);
+      person.appendChild(country);
     }
-
-}).catch(err => {
+  })
+  .catch(err => {
     // Do something for an error here
     console.log(err);
-})
+  });
